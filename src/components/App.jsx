9cb2768634f8827270f08236468114b5fc813964
CageyOwl <alex1377c9@gmail.com>
2023-04-21 01:35:32 +0300

    Homework: the first commit.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import css from './app.module.css';
+
+
+export function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm />
+
+      <h2 className={css.title}>Contacts</h2>
+      <Filter />
+      <ContactList />
     </div>
   );
-};
+}
---
+++ src/services/localStorageKeys.js
@@ -0,0 +1,4 @@
+const CONTACTS_KEY = 'contacts';
+const FILTER_KEY = 'filter';
+
+export { CONTACTS_KEY, FILTER_KEY };
